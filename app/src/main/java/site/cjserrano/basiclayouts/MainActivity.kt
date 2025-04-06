f0be392c29822faa68e2a80708829c97f4387a5b
Christopher Serrano <serrano.cjm@gmail.com>
2025-04-06 10:21:53 -0400

    body alignment

--- app/src/main/java/site/cjserrano/basiclayouts/MainActivity.kt
+++ app/src/main/java/site/cjserrano/basiclayouts/MainActivity.kt
@@ -4,13 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import site.cjserrano.basiclayouts.ui.theme.ComposeBasicLayoutsTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,6 +48,58 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+// Search bar
+@Composable
+fun AlignYourBodyElement(
+    @DrawableRes drawable: Int,
+    @StringRes text: Int,
+    modifier: Modifier = Modifier
+) {
+    // inside a column..,
+    // we align elements center horizontal
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally // Average alignment for a column (vertical)
+        // children elements will follow the parent's alignment, unless overridden with a modifier
+    ) {
+        // we load an image... with its modifiers and properties...
+        Image(
+            painter = painterResource(drawable),
+            contentDescription = null,
+            contentScale = ContentScale.Crop, // Just like a regular image elsewhere, Crop, Fit, Fill, FillHeight, FillWidth, Inside, FillBounds
+            modifier = Modifier
+                .size(88.dp)
+                .clip(CircleShape) // this defines the image shape in the layout
+                // there's also RoundedShape, RoundedCornerShape, etc
+                // These inherit from Shape/CornerBasedShape
+        )
+        // we set up a test, and place it using the baseline
+        // the baseline of a text is the line where text sits
+        // text should be aligned with the baseline instead of top/bottom
+        // baseline - top: 24dp (relative to element above)
+        // baseline - bottom: 8dp (relative to element below)
+        Text(
+            text = stringResource(text),
+            modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp),
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
+
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun AlignYourBodyElementPreview() {
+    ComposeBasicLayoutsTheme {
+        // we pass the assets via parameters
+        AlignYourBodyElement(
+            text = R.string.ab1_inversions,
+            drawable = R.drawable.ab1_inversions,
+            modifier = Modifier.padding(8.dp)
+        )
     }
 }
 
